57ae995d3d12fd69ce33dcb134c5916ab85d9c4e
Jerome Mohanan <2962518+jero2rome@users.noreply.github.com>
2024-05-15 21:49:19 +0000

    feat: Add video processing and audio summarization script

---
+++ QuickStarts/VideoProcessingAudioVisualSummarization.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from Utilities import process_video
+
+# We'll be using the OpenAI DevDay Keynote Recap video. You can review the video here: https://www.youtube.com/watch?v=h02ti0Bl6zk
+# Only 30secs of the video is processed in this example to keep under the 1min limit for OpenAI API
+VIDEO_PATH = "data/keynote_recap.mp4"
+
+# Load environment variables from .env file
+load_dotenv()
+
+MODEL="gpt-4o"
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+# Extract 1 frame per second. You can adjust the `seconds_per_frame` parameter to change the sampling rate
+base64Frames, audio_path = process_video(VIDEO_PATH, seconds_per_frame=1)
+
+# Transcribe the audio
+transcription = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=open(audio_path, "rb"),
+)
+
+## Generate a summary with visual and audio
+response = client.chat.completions.create(
+    model=MODEL,
+    messages=[
+    {"role": "system", "content":"""You are generating a video summary. Create a summary of the provided video and its transcript. Respond in Markdown"""},
+    {"role": "user", "content": [
+        "These are the frames from the video.",
+        *map(lambda x: {"type": "image_url", 
+                        "image_url": {"url": f'data:image/jpg;base64,{x}', "detail": "low"}}, base64Frames),
+        {"type": "text", "text": f"The audio transcription is: {transcription.text}"}
+        ],
+    }
+],
+    temperature=0,
+)
+print(response.choices[0].message.content)
